c56268ad2e30d14ec354ebbee2f37188bf096eee
Renat <renat_ismailov@bk.ru>
2021-09-09 00:24:38 +0300

    fix: Исправить валидацию

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
-const { validateUserId, validateUserUpdate } = require('../middlewares/validation');
+const { validateUserUpdate } = require('../middlewares/validation');
 const { getUser, updateUser } = require('../controllers/users');
 
-router.get('/me', validateUserId, getUser);
+router.get('/me', getUser);
 router.patch('/me', validateUserUpdate, updateUser);
 
 module.exports = router;
